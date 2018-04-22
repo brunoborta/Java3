14c3f3624399c4ef143fab8a53d678085dc1b5cd
Bruno Bortagaray <bruno.bortagaray@gmail.com>
2018-04-22 00:09:36 -0300

    Estilos, Mockups e o basico do basico do projeto final

---
+++ ProjetoFinal/Projeto/app/src/main/java/progweb3/poa/ifrs/edu/aula8/ListaCard.java
@@ -0,0 +1,13 @@
+package progweb3.poa.ifrs.edu.aula8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_card);
+    }
+}

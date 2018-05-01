cff5a00ad7a21cf3d7bae11e5e2238c1d64790ec
Bruno Bortagaray <bruno.bortagaray@gmail.com>
2018-05-01 00:10:49 -0300

    Adicionado o formulario para insercao de buracos

---
+++ ProjetoFinal/Projeto/app/src/main/java/progweb3/poa/ifrs/edu/aula8/BuracoFormularioActivity.java
@@ -0,0 +1,15 @@
+package progweb3.poa.ifrs.edu.aula8;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+
+public class BuracoFormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buraco_formulario);
+    }
+}
